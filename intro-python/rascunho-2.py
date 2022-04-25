c4c566ee3b652cd15622bba4eb3a1fdb0fb6ad9c
lisboamab <lisboamab@gmail.com>
2022-04-25 15:13:48 -0300

    segundo rascunho

---
+++ intro-python/rascunho-2.py
@@ -0,0 +1,12 @@
+respostas = []
+
+respostas.append(input(f"\nDigite seu nome:"))
+
+respostas.append(int(input(f"\nDigite sua idade")))
+
+respostas.append(input(f"\nDigite seu e-mail"))
+
+
+nome, idade, email = respostas
+
+print(f"\n Olá {nome} que legal, você tem {idade} anos! Posso confirmar o e-mail {email}?")
