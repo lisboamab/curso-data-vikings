342904b09531d07fd0b9f82a4f973b4432fab3cc
lisboamab <lisboamab@gmail.com>
2022-04-25 19:25:15 -0300

    adição de comentarios

--- intro-python/rascunho-2.py
+++ intro-python/rascunho-2.py
@@ -10,4 +10,5 @@ respostas.append(input(f"\nDigite seu e-mail"))
 
 nome, idade, email = respostas
 
+#o 'f' antes das aspas faz a função do antigo .format do python, possibilita fazer operações e colocar váriaveis dentro de strings.
 print(f"\n Olá {nome} que legal, você tem {idade} anos! Posso confirmar o e-mail {email}?")
--- intro-python/rascunho-3.py
+++ intro-python/rascunho-3.py
@@ -6,6 +6,11 @@ lista_teste = [0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for i in lista_teste:
     if i == lista_teste[i] + 1:
+        #pop() é um metodo para list que remove um elemento com base no index dele. Lembrando que em python a indexação inicia em 0.
         lista_teste.pop(i)
 
+#sort() é um metodo aplicado ao tipo list que ordena os elementos da lista, passando o parametro 'reverse=True' a ordem é invertida
+lista_teste.sort(reverse=True)
+
 print(lista_teste)
+
