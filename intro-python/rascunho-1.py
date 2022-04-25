31a1a6637d97b1459d32b68fbbe750494aa58ef7
lisboamab <lisboamab@gmail.com>
2022-04-25 15:02:34 -0300

    primeiro commit

---
+++ intro-python/rascunho-1.py
@@ -0,0 +1,16 @@
+#lista
+import email
+
+
+lista_exemplo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lista_exemplo)
+
+#tupla -> imutavel
+
+tupla_exemplo = (1, 2, 3, 4, 5)
+
+#dicionario 
+
+dicionario_exemplo = {'nome': "Marcos Lisboa", 'email': 'lisboamab@gmail.com', 'idade': 25, 'email': 'marcos.lisboa@stone.com.br' }
+
+print(dicionario_exemplo)
