0c5dd938688b0f5cec3d25f6ba2e7d95eec50374
lisboamab <lisboamab@gmail.com>
2022-04-27 18:40:43 -0300

    pacote time

---
+++ time/time-rascunho.py
@@ -0,0 +1,27 @@
+from time import *
+
+agora = localtime()
+
+print(strftime('%d/%m/%y %H:%M', agora))
+
+print("Agora vou contar até 10!")
+sleep(1)
+print("1")
+sleep(1)
+print("2")
+sleep(1)
+print("3")
+sleep(1)
+print("4")
+sleep(1)
+print("5")
+sleep(1)
+print("6")
+sleep(1)
+print("7")
+sleep(1)
+print("8")
+sleep(1)
+print("9")
+sleep(1)
+print("10")
