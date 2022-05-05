600eae11a46a9b7df92e3a9801a4afad97d5d1b9
lisboamab <lisboamab@gmail.com>
2022-05-04 21:25:05 -0300

    contador e teste de while

---
+++ time/teste-while.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+inicio = (int(input("De qual número você quer partir? ")))
+
+fim = (int(input("Digite até onde você quer contar ")))
+
+intervalo = (int(input("Qual é o intervalo que você quer entre os números contados? ")))
+
+if inicio <= fim:
+    while inicio <= fim:
+        print(inicio)
+        sleep(1)
+        inicio = inicio + intervalo
+    print("FIM!")
+elif inicio >= fim:
+    while inicio >= fim:
+        print(inicio)
+        sleep(1)
+        inicio = inicio - intervalo
+    print("FIM!")
